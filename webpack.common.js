56f4beabaa07362e65e375c095c82f6238d8a1ae
Yuji Tabata <yujitabata@icloud.com>
2021-03-27 16:16:31 +0900

    Add deploy workflow

--- webpack.common.js
+++ webpack.common.js
@@ -19,7 +19,7 @@ module.exports = {
     extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: 'game.js',
+    filename: 'pong.js',
     path: path.resolve(__dirname, 'dist'),
   }
 }
